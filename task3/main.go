6edc2c22972ce60ca1334daca85e500a7d4f7207
dynamomoskow <95101073+dynamomoskow@users.noreply.github.com>
2022-01-28 17:07:57 +0300

    edit

--- task3/main.go
+++ task3/main.go
@@ -5,16 +5,16 @@ import "fmt"
 func main() {
 	var chislo int
 	fmt.Scan(&chislo)
-	if chislo == 10000{
+	switch {
+	case chislo == 10000:
 		fmt.Println(chislo /10000)
-	}else if chislo >= 1000 {
+	case chislo >= 1000:
 		fmt.Println(chislo / 1000)
-	}else if chislo >= 100 {
+	case chislo >= 100:
 		fmt.Println(chislo / 100)
-	}else if chislo >= 10 {
+	case chislo >= 10:
 		fmt.Println(chislo / 10)
-	}else if chislo < 10 {
+	case chislo < 10:
 		fmt.Println(chislo)
 	}
-
 }
