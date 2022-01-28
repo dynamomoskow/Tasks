8fbfc8e1cdfe8d41cf73aa334a667e69fe5311c3
dynamomoskow <95101073+dynamomoskow@users.noreply.github.com>
2022-01-28 16:43:23 +0300

    add task3

---
+++ task3/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var chislo int
+	fmt.Scan(&chislo)
+	if chislo == 10000{
+		fmt.Println(chislo /10000)
+	}else if chislo >= 1000 {
+		fmt.Println(chislo / 1000)
+	}else if chislo >= 100 {
+		fmt.Println(chislo / 100)
+	}else if chislo >= 10 {
+		fmt.Println(chislo / 100)
+	}else if chislo < 10 {
+		fmt.Println(chislo)
+	}
+
+}
